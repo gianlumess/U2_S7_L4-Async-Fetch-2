df4ac36d68739a9c8bfd449d75a266849f56bd77
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-13 16:33:09 +0200

    creata funzione per immagini cani e implementato tasto "hide"

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const apiKey = "RcMqM6X6vDlI0vgXQK6uR6CyXW4QhvkcPA4zdWQ24JrOm06PnDXHTMxT";
-const url = "https://api.pexels.com/v1/search?query=cats";
-
+const catsUrl = "https://api.pexels.com/v1/search?query=cats";
+const dogsUrl = "https://api.pexels.com/v1/search?query=dogs";
 const headers = {
   Authorization: apiKey,
 };
 
 const catsImages = () => {
-  fetch(url, {
+  fetch(catsUrl, {
     method: "GET",
     headers: headers,
   })
@@ -34,15 +34,68 @@ const catsImages = () => {
         img.classList.add("card-img-top");
         img.src = photo.src.original;
 
-        card.appendChild(img);
+        const hideBtn = document.createElement("a");
+        hideBtn.classList.add("btn");
+        hideBtn.classList.add("btn-danger");
+        hideBtn.innerText = "Hide";
+        hideBtn.addEventListener("click", () => {
+          col.remove();
+        });
 
+        card.appendChild(img);
+        card.appendChild(hideBtn);
         col.appendChild(card);
         row.appendChild(col);
       });
     });
 };
 
+const dogsImages = () => {
+  fetch(dogsUrl, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    })
+    .then((dogs) => {
+      console.log(dogs.photos);
+
+      const row = document.getElementById("imgContainer");
+
+      dogs.photos.forEach((photo) => {
+        const col = document.createElement("div");
+        col.classList.add("col");
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.classList.add("card-img-top");
+        img.src = photo.src.original;
+
+        const hideBtn = document.createElement("a");
+        hideBtn.classList.add("btn");
+        hideBtn.classList.add("btn-danger");
+        hideBtn.innerText = "Hide";
+        hideBtn.addEventListener("click", () => {
+          col.remove();
+        });
+
+        card.appendChild(img);
+        card.appendChild(hideBtn);
+        col.appendChild(card);
+        row.appendChild(col);
+      });
+    });
+};
 const firstBtn = document.getElementById("firstBtn");
 const secondBtn = document.getElementById("secondBtn");
 
 firstBtn.addEventListener("click", catsImages);
+secondBtn.addEventListener("click", dogsImages);
