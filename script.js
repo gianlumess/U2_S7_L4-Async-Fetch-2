22e45212e55d5a1c6adabce17c71f328b281aef5
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-13 17:31:08 +0200

    ottimizzato codice utilizzando una sola funzione con parametro

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const headers = {
   Authorization: apiKey,
 };
 
-const catsImages = () => {
-  fetch(catsUrl, {
+const generateImages = (query) => {
+  fetch(`https://api.pexels.com/v1/search?query=${query}`, {
     method: "GET",
     headers: headers,
   })
@@ -18,12 +18,12 @@ const catsImages = () => {
         throw new Error("Network response was not ok");
       }
     })
-    .then((cats) => {
-      console.log(cats.photos);
+    .then((resObj) => {
+      console.log(resObj.photos);
 
       const row = document.getElementById("imgContainer");
 
-      cats.photos.forEach((photo) => {
+      resObj.photos.forEach((photo) => {
         const col = document.createElement("div");
         col.classList.add("col");
 
@@ -73,75 +73,16 @@ const catsImages = () => {
     .catch((err) => console.log(err));
 };
 
-const dogsImages = () => {
-  fetch(dogsUrl, {
-    method: "GET",
-    headers: headers,
-  })
-    .then((response) => {
-      console.log(response);
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Network response was not ok");
-      }
-    })
-    .then((dogs) => {
-      console.log(dogs.photos);
-
-      const row = document.getElementById("imgContainer");
-
-      dogs.photos.forEach((photo) => {
-        const col = document.createElement("div");
-        col.classList.add("col");
-
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        const img = document.createElement("img");
-        img.classList.add("card-img-top");
-        img.src = photo.src.original;
-
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.innerText = "Lorem Ipsum";
-
-        const cardText = document.createElement("p");
-        cardText.classList.add("card-text");
-        cardText.innerText =
-          "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.";
-
-        const hideBtn = document.createElement("a");
-        hideBtn.classList.add("btn");
-        hideBtn.classList.add("btn-danger");
-        hideBtn.innerText = "Hide";
-        hideBtn.addEventListener("click", () => {
-          col.remove();
-        });
-
-        const id = document.createElement("small");
-        id.classList.add("text-muted");
-        id.classList.add("ms-auto");
-        id.innerText = photo.id;
-
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardText);
-        cardBody.appendChild(hideBtn);
-        cardBody.appendChild(id);
-
-        card.appendChild(img);
-        card.appendChild(cardBody);
-        col.appendChild(card);
-        row.appendChild(col);
-      });
-    })
-    .catch((err) => console.log(err));
-};
 const firstBtn = document.getElementById("firstBtn");
 const secondBtn = document.getElementById("secondBtn");
+const row = document.getElementById("imgContainer");
 
-firstBtn.addEventListener("click", catsImages);
-secondBtn.addEventListener("click", dogsImages);
+firstBtn.addEventListener("click", () => {
+  row.innerHTML = "";
+  generateImages("mountains");
+});
+
+secondBtn.addEventListener("click", () => {
+  row.innerHTML = "";
+  generateImages("cars");
+});
